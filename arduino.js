f457b9015d82a258ac4ac4d6b2770f559e3f3bc7
Gunnar Ahlbom <Gahlbom@gmail.com>
2017-11-23 14:31:14 +0100

    sensor typo

--- arduino.js
+++ arduino.js
@@ -3,13 +3,13 @@ var board = new five.Board();
  
 board.on('ready', function() {
   var led = new five.Led(13); // pin 13
-  var moisture_sensor = new five.Sensor({
+  var sensor = new five.Sensor({
 	pin: "A0", 
 	freq: 250, 
 	threshold: 5
   });
 
-  moisture_sensor.on("change", () => {
+  sensor.on("change", () => {
     console.log('Sensor value: ', sensor.scaleTo(0, 10));
   });
 });
